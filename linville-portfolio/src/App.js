c54f35dcf2a8af5bd87ad8f79adb3c8473b1a1fe
Kelsey O'Kelley <km.okelley89@gmail.com>
2024-02-26 13:30:53 -0700

    create react app, install react-bootstrap

---
+++ linville-portfolio/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>
+          Sarah Susanne Linville
+          <br />
+          Journalist & Photographer
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
